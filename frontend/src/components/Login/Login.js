dbab4b6fe4dd445119e1dd6a045c055274f0d178
camila <camilailiberg@gmail.com>
2021-07-09 13:35:14 -0400

    organized exports into index. new navbar componenet

---
+++ frontend/src/components/Login/Login.js
@@ -0,0 +1,5 @@
+import "./Login.css";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+export default function Login() {}
---
+++ frontend/src/components/Register/Register.js
@@ -0,0 +1,11 @@
+import "./Register.css";
+
+import React from "react";
+
+export default function Register() {
+	return (
+		<div>
+			<h1>Register Component</h1>
+		</div>
+	);
+}
