58ec74c88a7c7e2e0bcbfad9a10e44e8f8040f47
camila <camilailiberg@gmail.com>
2021-07-16 09:39:40 -0400

    added search bar in list view. Not working yet

--- frontend/src/components/Lists/Lists.js
+++ frontend/src/components/Lists/Lists.js
@@ -1,7 +1,29 @@
 import "./Lists.css";
 import React from "react";
 import { ListCard } from "components";
+import InputBase from "@material-ui/core/InputBase";
+import { makeStyles } from "@material-ui/core/styles";
+import SearchIcon from "@material-ui/icons/Search";
+
+const useStyles = makeStyles((theme) => ({
+	inputRoot: {
+		color: "inherit",
+	},
+	inputInput: {
+		padding: theme.spacing(1, 1, 1, 0),
+		// vertical padding + font size from searchIcon
+		paddingLeft: `calc(1em + ${theme.spacing(3)}px)`,
+		transition: theme.transitions.create("width"),
+		width: "100%",
+		[theme.breakpoints.up("md")]: {
+			width: "30ch",
+		},
+		fontSize: "1.5em",
+	},
+}));
+
 function Lists() {
+	const classes = useStyles();
 	return (
 		<div className="Lists">
 			{/* <CssBaseline /> */}
@@ -16,17 +38,31 @@ function Lists() {
 			</div>
 			<div className="search-and-create">
 				<div className="search">
-					<h1>Search</h1>
+					<div className="search-bar">
+						{/* <h1>Search</h1> */}
+						<InputBase
+							placeholder="Search…"
+							classes={{
+								root: classes.inputRoot,
+								input: classes.inputInput,
+							}}
+							inputProps={{ "aria-label": "search" }}
+						/>
+					</div>
+					<button type="submit" className="search-icon">
+						<SearchIcon className="icon" />
+					</button>
 				</div>
+
 				<div className="create-new-list">
-					<h1>Create New +</h1>
+					<h1>Create New List +</h1>
 				</div>
 			</div>
 			<div className="display-lists-area">
-				{/* <ListCard />
 				<ListCard />
 				<ListCard />
-				<ListCard /> */}
+				<ListCard />
+				<ListCard />
 			</div>
 		</div>
 	);
