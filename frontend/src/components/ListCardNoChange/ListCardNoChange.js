5853d4c397a9c9e3071a7d8961020950f754b63c
SanCebar <r19velasco@gmail.com>
2021-07-30 09:29:41 -0700

    new icon covers

--- frontend/src/components/ListCardNoChange/ListCardNoChange.js
+++ frontend/src/components/ListCardNoChange/ListCardNoChange.js
@@ -1,6 +1,5 @@
 import "./ListCardNoChange.css";
 import React from "react";
-import { useAuthContext } from "contexts/auth";
 import { Link } from "react-router-dom";
 import { BiBookHeart } from "react-icons/bi";
 import { GoBook } from "react-icons/go";
@@ -10,28 +9,27 @@ import { FaBookDead } from "react-icons/fa"
 // const defaultBookCover = "https://source.unsplash.com/random";
 
 function ListCardNoChange({ list }) {
-  const { user } = useAuthContext();
+  const renderIcon = () => {
+	if (list?.list_name === "Want To Read") {
+		return <BiBookHeart />
+	} else if (list?.list_name === "Currently Reading") {
+		return (<GoBook />)
+	} else if (list?.list_name === "Did Not Finish") {
+		return (<FaBookDead />)
+	} else if (list?.list_name === "Finished") {
+		return (<RiBook2Fill />)
+	}
+  }
 
   return (
     <div className="ListCardNoChange">
       <div className="cover">
         <Link to={`/my-lists/${list.id}`}>
-          {/* <img alt="list cover" src={list?.image || defaultBookCover} />	 */}
-          {/* <BiBookHeart /> */}
-          {/* <GoBook /> */}
-		  {/* <RiBook2Fill /> */}
-		  <FaBookDead />
+			{renderIcon()}
         </Link>
       </div>
       <div className="information">
         <h2>{list?.list_name}</h2>
-        {/* <div className="by-and-more">
-					<div className="by-username">
-						By{" "} 
-						<br />
-						{user?.username}
-					</div>
-				</div> */}
       </div>
     </div>
   );
