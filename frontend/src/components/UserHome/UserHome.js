0e1922c0e9ea25a32e670fdaff4bc10f40e440bb
camila <camilailiberg@gmail.com>
2021-08-04 12:42:42 -0400

    deleted some comments

--- frontend/src/components/UserHome/UserHome.js
+++ frontend/src/components/UserHome/UserHome.js
@@ -4,82 +4,80 @@ import { Link } from "react-router-dom";
 import apiClient from "services/apiClient";
 import { BookPreview } from "components";
 
-const defaultBookCover = 
-  "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.calendarclub.co.uk%2F-%2Fmedia%2Fproductimages%2F18%2F66%2F186694_main.jpg&f=1&nofb=1";
+const defaultBookCover =
+	"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.calendarclub.co.uk%2F-%2Fmedia%2Fproductimages%2F18%2F66%2F186694_main.jpg&f=1&nofb=1";
 
 export default function UserHome({ topSellers = [] }) {
-  const [error, setError] = useState(null);
-  const [bookList, setBookList] = useState([]);
-  let currentlyReadingListId = "";
+	const [error, setError] = useState(null);
+	const [bookList, setBookList] = useState([]);
+	let currentlyReadingListId = "";
 
-  useEffect(() => {
-    const fetchBooksInList = async (listId) => {
-      const { data, error } = await apiClient.getBooksInList(listId);
-      if (error) {
-        setError(error);
-      }
-      if (data?.books_in_list) {
-        setError(null);
-        setBookList(data.books_in_list);
-      }
-    };
+	useEffect(() => {
+		const fetchBooksInList = async (listId) => {
+			const { data, error } = await apiClient.getBooksInList(listId);
+			if (error) {
+				setError(error);
+			}
+			if (data?.books_in_list) {
+				setError(null);
+				setBookList(data.books_in_list);
+			}
+		};
 
-    const fetchCurrentlyReadingByUserId = async () => {
-      const { data, error } = await apiClient.getCurrentlyReadingListByUserId();
-      if (error) {
-        setError(error);
-      }
-      if (data?.currently_reading) {
-        setError(null);
-        currentlyReadingListId = data.currently_reading.id;
-        console.log(data.currently_reading);
-        console.log(fetchBooksInList(currentlyReadingListId));
-        fetchBooksInList(currentlyReadingListId);
-      }
-    };
+		const fetchCurrentlyReadingByUserId = async () => {
+			const { data, error } = await apiClient.getCurrentlyReadingListByUserId();
+			if (error) {
+				setError(error);
+			}
+			if (data?.currently_reading) {
+				setError(null);
+				currentlyReadingListId = data.currently_reading.id;
+				fetchBooksInList(currentlyReadingListId);
+			}
+		};
 
-    fetchBooksInList();
-    fetchCurrentlyReadingByUserId();
-  }, []);
+		fetchBooksInList();
+		fetchCurrentlyReadingByUserId();
+	}, []);
 
-  return (
-    <div className="UserHome">
-      <div className="top-feed">
-        <h2>Top Sellers</h2>
+	return (
+		<div className="UserHome">
+			<div className="top-feed">
+				<h2>Top Sellers</h2>
 
-        {/* CSS styling for home-feed in Home.css to avoid repetition */}
-        <div className="home-feed-books">
-          {topSellers.map((book) => (
-            <div className="preview">
-              <Link to={`/books/top/sellers/${book.title}`}>
-                <img
-                  alt="book cover"
-                  src={book?.book_image || defaultBookCover}
-                />
-              </Link>
-              <div className="book-details">
-                <h3>{book.title}</h3>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
+				{/* CSS styling for home-feed in Home.css to avoid repetition */}
+				<div className="home-feed-books">
+					{topSellers.map((book) => (
+						<div className="preview">
+							<Link to={`/books/top/sellers/${book.title}`}>
+								<img
+									alt="book cover"
+									src={book?.book_image || defaultBookCover}
+								/>
+							</Link>
+							<div className="book-details">
+								<h3>{book.title}</h3>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
 
-      <div className="home-feed">
-        <h2>Currently Reading</h2>
+			<div className="home-feed">
+				<h2>Currently Reading</h2>
 
-        {/* {(isEmpty===true) ? (
+				{/* {(isEmpty===true) ? (
                     <div className="empty-message">
                         <h2>Your list doesn't have any books in it! Add books to change this.</h2>
                     </div>
                 ) : ( */}
-        <div className="home-feed-books">
-          {bookList.map((book) => (
-            <BookPreview book={book} key={book.title} />
-          ))}
-        </div>
-        {/* )} */}
-      </div>
-    </div>
-  );
+				<div className="home-feed-books">
+					{bookList.map((book) => (
+						<BookPreview book={book} key={book.title} />
+					))}
+				</div>
+				{/* )} */}
+			</div>
+		</div>
+	);
 }
