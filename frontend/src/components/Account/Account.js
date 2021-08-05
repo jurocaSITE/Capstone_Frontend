d50c3930b7d9041e91041ac33ecfc46e19309f2b
camila <camilailiberg@gmail.com>
2021-08-05 15:56:12 -0400

    users now see a success message when updating their genre interests

--- frontend/src/components/Account/Account.js
+++ frontend/src/components/Account/Account.js
@@ -12,22 +12,22 @@ function Account() {
 	const [errors, setErrors] = useState({});
 	const [toggleState, setToggleState] = useState(1);
 
-  const toggleTab = (index) => {
-    setToggleState(index);
-  };
+	const toggleTab = (index) => {
+		setToggleState(index);
+	};
 
-  const handleOnSignOut = async () => {
-    await apiClient.logoutUser();
-    setUser(null);
-    setErrors(null);
-    navigate("/");
-  };
+	const handleOnSignOut = async () => {
+		await apiClient.logoutUser();
+		setUser(null);
+		setErrors(null);
+		navigate("/");
+	};
 
-  const handleOneDelete = async (event) => {
-    await apiClient.deleteUserProfile();
+	const handleOneDelete = async (event) => {
+		await apiClient.deleteUserProfile();
 
-    handleOnSignOut();
-  };
+		handleOnSignOut();
+	};
 
 	if (!user?.email) {
 		return <NotAllowed />;
@@ -38,105 +38,92 @@ function Account() {
 				<h1>Account</h1>
 				<p>Update your profile and set your account preferences</p>
 			</section>
-			{/*<section className="card-content">
-				<div className="card">
-					<h1>hola</h1>
-					<h1>hola</h1>
+			<div className="container">
+				<div className="bloc-tabs">
+					<button
+						className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
+						onClick={() => toggleTab(1)}
+					>
+						General
+					</button>
+					<button
+						className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
+						onClick={() => toggleTab(2)}
+					>
+						Profile
+					</button>
+					<button
+						className={toggleState === 3 ? "tabs active-tabs" : "tabs"}
+						onClick={() => toggleTab(3)}
+					>
+						Update Interests
+					</button>
+					<button
+						className={toggleState === 4 ? "tabs active-tabs" : "tabs"}
+						onClick={() => toggleTab(4)}
+					>
+						Delete account
+					</button>
 				</div>
-			</section> */}
-      <div className="container">
-        <div className="bloc-tabs">
-          <button
-            className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
-            onClick={() => toggleTab(1)}
-          >
-            General
-          </button>
-          <button
-            className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
-            onClick={() => toggleTab(2)}
-          >
-            Profile
-          </button>
-          <button
-            className={toggleState === 3 ? "tabs active-tabs" : "tabs"}
-            onClick={() => toggleTab(3)}
-          >
-            Update Interests
-          </button>
-          <button
-            className={toggleState === 4 ? "tabs active-tabs" : "tabs"}
-            onClick={() => toggleTab(4)}
-          >
-            Delete account
-          </button>
-        </div>
 
-        <div className="content-tabs">
-          <div
-            className={
-              toggleState === 1 ? "content  active-content" : "content"
-            }
-          >
-            {/* <h2>General</h2> */}
-            <General className="general" />
-          </div>
+				<div className="content-tabs">
+					<div
+						className={
+							toggleState === 1 ? "content  active-content" : "content"
+						}
+					>
+						{/* <h2>General</h2> */}
+						<General className="general" />
+					</div>
 
-          <div
-            className={
-              toggleState === 2 ? "content  active-content" : "content"
-            }
-          >
-            {/* <h2>Profile</h2>
-						<hr /> */}
-            {/* <p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
-						voluptatum qui adipisci.
-					</p> */}
+					<div
+						className={
+							toggleState === 2 ? "content  active-content" : "content"
+						}
+					>
+						<EditProfile className="edit-profile" />
+					</div>
 
-            <EditProfile className="edit-profile" />
-          </div>
+					<div
+						className={
+							toggleState === 3 ? "content  active-content" : "content"
+						}
+					>
+						<h3>Update Genre Interests</h3>
+						<hr />
+						<p>Click the link below to modify your preferred genres.</p>
+						<ActionButton
+							altClass={`update-interests-btn`}
+							text={`Update Interests`}
+							link={`/update-interests`}
+						/>
+					</div>
 
-          <div
-            className={
-              toggleState === 3 ? "content  active-content" : "content"
-            }
-          >
-            <h3>Update Genre Interests</h3>
-            <hr />
-            <p>Click the link below to modify your preferred genres.</p>
-            <ActionButton
-              altClass={`update-interests-btn`}
-              text={`Update Interests`}
-              link={`/update-interests`}
-            />
-          </div>
-
-          <div
-            className={
-              toggleState === 4 ? "content  active-content" : "content"
-            }
-          >
-            <div className="delete-btn">
-              <p className="delete-account-body">
-                Please note that this is a permanent action. All of your data on
-                the website (lists, ratings, reviews) will be lost forever.{" "}
-                <br /> Are you sure you want to delete your account?
-              </p>
-              <Link to="/">
-                <button
-                  className="btn delete-account"
-                  onClick={handleOneDelete}
-                >
-                  Yes, Delete Account
-                </button>
-              </Link>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+					<div
+						className={
+							toggleState === 4 ? "content  active-content" : "content"
+						}
+					>
+						<div className="delete-btn">
+							<p className="delete-account-body">
+								Please note that this is a permanent action. All of your data on
+								the website (lists, ratings, reviews) will be lost forever.{" "}
+								<br /> Are you sure you want to delete your account?
+							</p>
+							<Link to="/">
+								<button
+									className="btn delete-account"
+									onClick={handleOneDelete}
+								>
+									Yes, Delete Account
+								</button>
+							</Link>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Account;
--- frontend/src/components/UpdateGenreInterests/UpdateGenreInterests.js
+++ frontend/src/components/UpdateGenreInterests/UpdateGenreInterests.js
@@ -9,73 +9,88 @@ import { ActionButton } from "components";
 import { genres } from "services/genres";
 
 export default function UpdateGenreInterests() {
-	const navigate = useNavigate()
-  const { user, setUser } = useAuthContext();
-  const [interests, setInterests] = useState({});
-  const [errors, setErrors] = useState({});
+	const navigate = useNavigate();
+	const { user, setUser } = useAuthContext();
+	const [interests, setInterests] = useState({});
+	const [errors, setErrors] = useState({});
+	const [successMessage, setSuccessMessage] = useState(false);
 
-  const handleCheck = (e) => {
-    const { name, checked } = e.target;
-    if (checked) {
-      setInterests((i) => ({ ...i, [name]: checked }));
-    }
-    if (!checked) {
-      setInterests((i) => ({ ...i, [name]: checked }));
-    }
-  };
+	const handleCheck = (e) => {
+		const { name, checked } = e.target;
+		if (checked) {
+			setInterests((i) => ({ ...i, [name]: checked }));
+		}
+		if (!checked) {
+			setInterests((i) => ({ ...i, [name]: checked }));
+		}
+	};
 
-  const handleOnSubmit = async () => {
-    const finalInterests = Object.keys(interests).filter((x) => interests[x]);
-    console.log("Submitting...finalInterests...", finalInterests);
-    setErrors((e) => ({ ...e, form: null }));
+	const handleOnSubmit = async () => {
+		const finalInterests = Object.keys(interests).filter((x) => interests[x]);
+		console.log("Submitting...finalInterests...", finalInterests);
+		setErrors((e) => ({ ...e, form: null }));
 
-    const { data, error } = await apiClient.updateUserInterests({
-      genre_interests: finalInterests,
-    });
-    if (error) {
-      alert(`Error!\n ${error}`);
-      setErrors((e) => ({ ...e, form: error }));
-    }
-    if (data?.genre_interests) {
-      setErrors((e) => ({ ...e, form: null }));
-      setUser((u) => ({ ...u, genre_interest: data.genre_interests }));
-	  navigate("/")
-    }
-  };
+		const { data, error } = await apiClient.updateUserInterests({
+			genre_interests: finalInterests,
+		});
+		if (error) {
+			alert(`Error!\n ${error}`);
+			setErrors((e) => ({ ...e, form: error }));
+		}
+		if (data?.genre_interests) {
+			setErrors((e) => ({ ...e, form: null }));
+			setUser((u) => ({ ...u, genre_interest: data.genre_interests }));
+			setSuccessMessage(true);
+			setTimeout(() => {
+				setSuccessMessage(false);
+			}, 3000);
+			// navigate("/account");
+		}
+	};
 
-  return (
-    <div className="UpdateGenreInterests">
-      <h1>Select Interests</h1>
-      {!user && (
-        <>
-          <h2>Unauthorized User</h2>
-        </>
-      )}
-      {user && (
-        <>
-          <form className="interests-container">
-            {genres?.map((x) => (
-              <label
-                className={`interest-card ${interests[x] ? "checked" : ""}`}
-                htmlFor={x}
-                key={x}
-              >
-                <input
-                  type="checkbox"
-                  id={x}
-                  name={x}
-                  value={x}
-                  onChange={handleCheck}
-                />
-                {x}
-                {interests[x] ? <FaCheckCircle /> : <FaRegCheckCircle />}
-              </label>
-            ))}
-          </form>
-
-          <ActionButton text={`Submit`} clickFunc={handleOnSubmit} />
-        </>
-      )}
-    </div>
-  );
+	return (
+		<div className="UpdateGenreInterests">
+			<h1>Select Interests</h1>
+			{!user && (
+				<>
+					<h2>Unauthorized User</h2>
+				</>
+			)}
+			{successMessage && (
+				<span className="success-message">
+					{successMessage ? "Sucessfully updated" : ""}
+				</span>
+			)}
+			<br />
+			{user && (
+				<>
+					<form className="interests-container">
+						{genres?.map((x) => (
+							<label
+								className={`interest-card ${interests[x] ? "checked" : ""}`}
+								htmlFor={x}
+								key={x}
+							>
+								<input
+									type="checkbox"
+									id={x}
+									name={x}
+									value={x}
+									onChange={handleCheck}
+								/>
+								{x}
+								{interests[x] ? <FaCheckCircle /> : <FaRegCheckCircle />}
+							</label>
+						))}
+					</form>
+					<div className="buttons">
+						<div className="go-back">
+							<ActionButton text={`Go Back to Account`} link={`/account`} />
+						</div>
+						<ActionButton text={`Submit`} clickFunc={handleOnSubmit} />
+					</div>
+				</>
+			)}
+		</div>
+	);
 }
